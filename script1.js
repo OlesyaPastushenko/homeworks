d18b9eaa4d2d082bf2e3cf1913db358ff66bb598
OlesyaPastushenko <103423216+OlesyaPastushenko@users.noreply.github.com>
2022-05-08 20:10:56 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,14 +1,14 @@
 // Задача 1. Напишите функцию toSum для вычисления суммы массива целых чисел.
-sum = 0
+var sum = 0;
 function toSum(arr,i) {
     if(Number.isInteger(arr[i])){
         if(i === arr.length){
-            return sum
+            sum = sum + 0;
         } else {
-            sum = sum + arr[i] + toSum(arr,i+1)
+            sum = arr[i] + toSum(arr,i+1);
         }
     }
-    return sum
+    return sum;
 }
 console.log(toSum([2, 3, 4, 5, 6, 7],0));
 
