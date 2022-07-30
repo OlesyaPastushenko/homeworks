9f29f91f93dc6f67f9c2e551edbf22833f103ed5
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-07-30 15:43:38 +0300

    Create UserPairs component

--- src/components/List/list.js
+++ src/components/List/list.js
@@ -1,10 +1,14 @@
-import { MDCDataTable } from '@material/data-table';
+import { MDCDataTable } from "@material/data-table";
 import { createElem } from "../../helper/createElem";
+import UserPairs from "../UserPairs";
+const token = ""; // add link token https://github.com/settings/tokens
 
 const List = () => {
-    const root = document.getElementById('root');
-
-    createElem('div', root, `
+  const root = document.getElementById("root");
+  createElem(
+    "div",
+    root,
+    `
     <div class="mdc-data-table">
     <div class="mdc-data-table__table-container">
         <table class="mdc-data-table__table" aria-label="Students">
@@ -18,32 +22,49 @@ const List = () => {
         </table>
     </div>
     </div>
-    `, 'wrapper');
+    `,
+    "wrapper"
+  );
 
-    const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
-    const { content } = dataTable;
+  const dataTable = new MDCDataTable(document.querySelector(".mdc-data-table"));
+  const { content } = dataTable;
 
-    let users = [];
-    fetch('https://api.github.com/users')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(el => {
-            fetch(el.url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                users.push({
-                    login: data.login,
-                    followers: data.followers
-                });
+  let users = [];
+  fetch("https://api.github.com/users", {
+    headers: new Headers({
+      Authorization: "token " + token,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      Promise.all(
+        data.map((el) => {
+          return fetch(el.url, {
+            headers: new Headers({
+              Authorization: "token " + token,
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              users.push({
+                login: data.login,
+                followers: data.followers,
+              });
 
-                createElem('tr', content, `
+              createElem(
+                "tr",
+                content,
+                `
                 <th class="mdc-data-table__cell" scope="row">${data.login}</th>
-                <td class="mdc-data-table__cell mdc-data-table__cell--numeric">${data.followers}</td>`, 'mdc-data-table__row');
-                console.log(users)
+                <td class="mdc-data-table__cell mdc-data-table__cell--numeric">${data.followers}</td>`,
+                "mdc-data-table__row"
+              );
             });
-        });
+        })
+      ).then(() => {
+        UserPairs(users);
+      });
     });
-}
+};
 
 export default List;
